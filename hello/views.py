7bb9b3006dd9d33c5e471887443d48bc41f6f70e
Kiyoshi Kawana <kivantium@gmail.com>
2020-04-12 15:06:35 +0900

    Add timeline

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from social_django.models import UserSocialAuth
+from django.conf import settings
+
+import tweepy
 
 def index(request):
     if request.user.is_authenticated:
         user = UserSocialAuth.objects.get(user_id=request.user.id)
-        return render(request,'hello/index.html', {'user': user})
+        consumer_key = settings.SOCIAL_AUTH_TWITTER_KEY
+        consumer_secret = settings.SOCIAL_AUTH_TWITTER_SECRET
+        access_token = user.extra_data['access_token']['oauth_token']
+        access_secret = user.extra_data['access_token']['oauth_token_secret']
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_secret)
+        api = tweepy.API(auth)
+        timeline = api.home_timeline()
+        return render(request,'hello/index.html', {'user': user, 'timeline': timeline})
     else:
         return render(request,'hello/index.html')
