99b3210cc65666644dd310b050bbf1315ed876ef
Kiyoshi Kawana <kivantium@gmail.com>
2020-04-23 23:52:14 +0900

    Add illustlation only timeline

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,23 @@
+import os
+import re
+import urllib.request
+from urllib.parse import urlparse
+from PIL import Image
+from joblib import dump, load
+import tweepy
+
 from django.shortcuts import render
 from social_django.models import UserSocialAuth
 from django.conf import settings
+import more_itertools
 
-import tweepy
+import sys
+sys.path.append(os.path.dirname(__file__))
+import i2v
+
+illust2vec = i2v.make_i2v_with_onnx(os.path.join(os.path.dirname(__file__), "illust2vec_ver200.onnx"))
+
+clf = load(os.path.join(os.path.dirname(__file__), "clf.joblib"))
 
 def index(request):
     if request.user.is_authenticated:
@@ -14,7 +29,41 @@ def index(request):
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_secret)
         api = tweepy.API(auth)
-        timeline = api.home_timeline()
-        return render(request,'hello/index.html', {'user': user, 'timeline': timeline})
+        timeline = api.home_timeline(count=200, tweet_mode = 'extended')
+
+        tweet_illust = []
+        for tweet in timeline:
+            if 'media' in tweet.entities:
+                media  = tweet.extended_entities['media'][0]
+                media_url = media['media_url']
+                filename = os.path.basename(urlparse(media_url).path)
+                filename = os.path.join(os.path.dirname(__file__), 'images', filename)
+                urllib.request.urlretrieve(media_url, filename)
+                img = Image.open(filename)
+                feature = illust2vec.extract_feature([img])
+                prob = clf.predict_proba(feature)[0]
+                if prob[1] > 0.4:
+                    if hasattr(tweet, "retweeted_status"): 
+                        profile_image_url = tweet.retweeted_status.author.profile_image_url_https
+                        author = {'name': tweet.retweeted_status.author.name,
+                                  'screen_name': tweet.retweeted_status.author.screen_name}
+                        id_str = tweet.retweeted_status.id_str
+                    else:
+                        profile_image_url = tweet.author.profile_image_url_https
+                        author = {'name': tweet.author.name,
+                                  'screen_name': tweet.author.screen_name}
+                        id_str = tweet.id_str
+                    try:
+                        text = tweet.retweeted_status.full_text
+                    except AttributeError:
+                        text = tweet.full_text
+                    text = re.sub(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+$", '', text).rstrip()
+                    tweet_illust.append({'id_str': id_str, 
+                                         'profile_image_url': profile_image_url,
+                                         'author': author,
+                                         'text': text,
+                                         'image_url': media_url})
+        tweet_illust_chunked = list(more_itertools.chunked(tweet_illust, 4))
+        return render(request,'hello/index.html', {'user': user, 'timeline_chunked': tweet_illust_chunked})
     else:
         return render(request,'hello/index.html')
