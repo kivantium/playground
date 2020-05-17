cb85c9ae3318e4194381ace6d82757b8211ec507
Kiyoshi Kawana <kivantium@gmail.com>
2020-05-17 16:25:49 +0900

    Remove timeline

--- hello/urls.py
+++ hello/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('delete/<int:status_id>/', views.delete, name='delete'),
     path('set_i2v_tag/<int:status_id>/', views.set_i2v_tag, name='set_i2v_tag'),
     path('search/', views.search, name='search'),
-    path('timeline/', views.timeline, name='timeline'),
     path('logout/',
         django.contrib.auth.views.LogoutView.as_view(template_name = 'hello/logout.html'),
         name='logout'),
--- hello/views.py
+++ hello/views.py
@@ -28,24 +28,6 @@ def index(request):
 def about(request):
     return render(request, 'hello/about.html')
 
-def timeline(request):
-    lists = []
-    if request.user.is_authenticated:
-        try:
-            user = UserSocialAuth.objects.get(user_id=request.user.id)
-        except:
-            return redirect('/logout')
-        consumer_key = settings.SOCIAL_AUTH_TWITTER_KEY
-        consumer_secret = settings.SOCIAL_AUTH_TWITTER_SECRET
-        access_token = user.extra_data['access_token']['oauth_token']
-        access_secret = user.extra_data['access_token']['oauth_token_secret']
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_secret)
-        api = tweepy.API(auth)
-        lists = api.lists_all(
-            screen_name=user.extra_data['access_token']['screen_name'])
-    return render(request, 'hello/timeline.html', {'lists': lists})
-
 def ranking(request):
     now = datetime.datetime.now(pytz.timezone('UTC'))
     td = datetime.timedelta(hours=24)
