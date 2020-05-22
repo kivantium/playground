70599962eea87f063d03413a20dad61fb36e6cf8
Kiyoshi Kawana <kivantium@gmail.com>
2020-05-22 16:32:24 +0900

    Update admin tools

--- hello/admin.py
+++ hello/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Tag, ImageEntry
 
 # Register your models here.
+class ImageEntryAdmin(admin.ModelAdmin):
+    search_fields = ('status_id', )
+    filter_horizontal = ('tags', )
+
 admin.site.register(Tag)
-admin.site.register(ImageEntry)
+admin.site.register(ImageEntry, ImageEntryAdmin)
